dd23b51b99891c24c1b0b82c6abd56d1691b6a07
Han Seang Tae <sthan8832@gmail.com>
2019-01-08 11:21:36 +0900

    php로 만들기

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP TEST</title>
+  </head>
+  <body>
+    <?php
+    $str = "hellow PHP";
+     ?>
+     PHP 명령어로 작성한 <?= $str ?></br>
+     HTML 태그로 작성한 HELLOW PHP!!
+  </body>
+</html>
